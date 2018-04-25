92d6484f977a451f24dd6664070bc51b65c9b1b8
Lennart Brandt <lennart@elbstack.com>
2018-04-25 18:20:12 +0200

    Added website title and description

--- static.config.js
+++ static.config.js
@@ -1,50 +1,54 @@
-import axios from 'axios'
-import React, { Component } from 'react'
-import { ServerStyleSheet } from 'styled-components'
+import axios from "axios";
+import React, { Component } from "react";
+import { ServerStyleSheet } from "styled-components";
 
 export default {
-  siteRoot: 'https://lennartbrandt.com',
+  siteRoot: "https://lennartbrandt.com",
   getSiteData: () => ({
-    title: 'React Static',
+    title: "Lennart Brandt | Design X Entrepreneurship",
+    metaDescription:
+      "Hi! I'm Lennart, an enthusiastic product designer with a huge passion for entrepreneurship. Currently, I'm working with elbstack on exploring new business models within the space of Fitness and Blockchain. You can find my design work on Dribbble, follow me behind the scenes via Instagram and explore my daily thoughts on Twitter."
   }),
   getRoutes: async () => {
     return [
       {
-        path: '/',
-        component: 'src/containers/Home',
+        path: "/",
+        component: "src/containers/Home"
       },
       {
-        path: '/imprint',
-        component: 'src/containers/Imprint',
+        path: "/imprint",
+        component: "src/containers/Imprint"
       },
       {
         is404: true,
-        component: 'src/containers/404',
-      },
-    ]
+        component: "src/containers/404"
+      }
+    ];
   },
   renderToHtml: (render, Comp, meta) => {
-    const sheet = new ServerStyleSheet()
-    const html = render(sheet.collectStyles(<Comp />))
-    meta.styleTags = sheet.getStyleElement()
-    return html
+    const sheet = new ServerStyleSheet();
+    const html = render(sheet.collectStyles(<Comp />));
+    meta.styleTags = sheet.getStyleElement();
+    return html;
   },
   Document: class CustomHtml extends Component {
-    render () {
-      const {
-        Html, Head, Body, children, renderMeta,
-      } = this.props
-
+    render() {
+      const { Html, Head, Body, children, renderMeta, siteData } = this.props;
       return (
         <Html>
           <Head>
+            <title>{siteData.title}</title>
             <meta charSet="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="description" content={siteData.metaDescription} />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1"
+            />
             {renderMeta.styleTags}
           </Head>
           <Body>{children}</Body>
         </Html>
-      )
+      );
     }
-  },
-}
+  }
+};
